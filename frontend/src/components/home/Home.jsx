d4e84f4841b3eea69265c5dec3c6db8303a71f2f
alain-nambi <alainnambi@gmail.com>
2024-08-30 18:11:09 +0300

    feat : Handle logout

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Button } from '@mantine/core'
+import { useNavigate } from 'react-router-dom'
+import AuthContext from "../../context/AuthContext"
 
 const Home = () => {
-  console.log("Hello")
+  const navigate = useNavigate()
+  const { logout } = useContext(AuthContext)
+
+  const handleSignOff = (e) => {
+    e.preventDefault()
+    logout()
+    navigate('/signin')
+  }
+
   return (
     <div>
       <p>Bienvenu dans la page d'acceuil</p>
+      <Button onClick={handleSignOff}>Déconnecter</Button>
     </div>
   )
 }
