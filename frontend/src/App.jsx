8806386799152da228e7924a2a195a479cf845e5
alain-nambi <alainnambi@gmail.com>
2024-08-31 14:43:59 +0300

    fix : Redirection with AuthRedirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,64 @@
 import "@mantine/core/styles.css";
-
 import { MantineProvider } from "@mantine/core";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Link,
-  useNavigate,
+  Navigate,
 } from "react-router-dom";
 import SignUp from "./components/user/SignUp.jsx";
 import SignIn from "./components/user/SignIn.jsx";
 import Home from "./components/home/Home.jsx";
-import { Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { useContext } from "react";
+import AuthContext from "./context/AuthContext.jsx";
 
 export default function App() {
   return (
     <MantineProvider>
       <AuthProvider>
         <Router>
-            <Routes>
-              <Route path="/signin" element={<SignIn />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route
-                path="/home"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/" element={<Navigate to={"/home"} />} />
-              <Route path="*" element={<SignIn />} />{" "}
-              {/* Redirect to SignIn by default */}
-            </Routes>
+          <Routes>
+            <Route
+              path="/signin"
+              element={
+                <AuthRedirect>
+                  <SignIn />
+                </AuthRedirect>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <AuthRedirect>
+                  <SignUp />
+                </AuthRedirect>
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/" element={<Navigate to={"/home"} />} />
+            <Route path="*" element={<Navigate to={"/home"} />} />
+          </Routes>
         </Router>
       </AuthProvider>
     </MantineProvider>
   );
+}
+
+function AuthRedirect({ children }) {
+  const { auth } = useContext(AuthContext);
+
+  if (auth.token) {
+    return <Navigate to="/home" />;
+  }
+
+  return children;
 }
